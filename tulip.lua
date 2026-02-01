768b71e046fbaec5f970f4d34848ba9ba90940de
devenoch <durustein@gmail.com>
2026-02-01 18:54:38 -0500

    Create tulip.lua

---
+++ tulip.lua
@@ -0,0 +1,45 @@
+-- Tulip scripting language
+standard_functions = require 'standardlib'
+
+local filename = arg[1]
+if not filename then
+    print("Usage: tulip <script.tulip>")
+    return
+end
+
+local lcount = 0
+local varstorage = {}
+
+for line in io.lines(filename) do
+    lcount = lcount + 1
+
+    -- variable definitions
+    for key, value in line:gmatch("<(.-)>%[([^%]]+)%]") do
+        varstorage[key] = value
+    end
+
+    -- quoted words
+    for word in line:gmatch("'(.-)'") do
+        print(varstorage[word] or word)
+    end
+
+    -- variable usage
+    for out in line:gmatch("&(.-)&") do
+        if out == "" then
+            error("error on line " .. lcount .. ": empty variable reference")
+        end
+        if not varstorage[out] then
+            error("error on line " .. lcount .. ": variable " .. out .. " does not exist")
+        end
+        print(varstorage[out])
+    end
+
+    -- function calls
+    for func, param in line:gmatch("!(.-)!([%d%a]+)") do
+        if func == "memprint" then
+            standard_functions.memprint(tonumber(param))
+        elseif func == "wait" then
+            standard_functions.wait(tonumber(param))
+        end
+    end
+end
